9de331bcc42da0c4353149a671befb81bd1a141e
Pedro Felix <pedrofelixnunes.j@gmail.com>
2021-01-14 14:14:34 -0300

    exercicio_Vectores_refClass

---
+++ src/application/Program.java
@@ -0,0 +1,39 @@
+package application;
+
+import java.util.Scanner;
+
+import entities.Room;
+
+public class Program {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		Room[] vect = new Room[10];
+		
+		System.out.print("How many rooms will be rented? ");
+		int n = sc.nextInt();
+		
+		for (int i = 1; i <=n; i++) {
+			sc.nextLine();
+			System.out.println("Rent #"+ i + ":");
+			System.out.print("Name: ");
+			String name = sc.nextLine();
+			System.out.print("Email: ");
+			String email = sc.nextLine();
+			System.out.print("Room: ");
+			int room = sc.nextInt();
+			vect[room] = new Room(name, email);
+			
+		}
+		
+		System.out.println();
+		System.out.println("Busy rooms:");
+		for (int i = 0; i < 10; i++) {
+			if(vect[i] != null) {
+				System.out.println(i + ": " + vect[i] );
+			}	
+		}
+	
+		sc.close();
+	}
+}
